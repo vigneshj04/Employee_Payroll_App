d5faa2ea4b042f73b008b2d5a7f7d6d3948446f7
vigneshj04 <vigneshjmax@gmail.com>
2022-05-26 10:19:06 +0530

    Added: On Reset Attribute To Form Tag

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -104,3 +104,33 @@ function createAndUpdateStorage(employeePayrollData) {
   alert(employeePayrollList.toString());
   localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
+
+const resetForm = () => {
+  setValue('#name', '');
+  unsetSelectedValues('[name=profile]');
+  unsetSelectedValues('[name=gender]');
+  unsetSelectedValues('[name=department]');
+  setValue('#salary', '');
+  setTextValue('.salary-output', 400000);
+  setValue('#day', '1');
+  setValue('#month', 'January');
+  setValue('#year', '2021');
+  setValue('#notes', '');
+}
+
+const unsetSelectedValues = (propertyValue) => {
+  let allItems = document.querySelectorAll(propertyValue);
+  allItems.forEach(item => {
+      item.checked = false;
+  });
+}
+
+const setTextValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.textContent = value;
+}
+
+const setValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.value = value;
+}
